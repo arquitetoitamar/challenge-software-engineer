a20b22cfbd50091f3aa5b910a63745c1c68f2062
itamar_santos <isantos.ads@gmail.com>
2025-01-21 02:29:37 -0300

    fix create db script

--- infra/modules/rds/create_table.sql
+++ infra/modules/rds/create_table.sql
@@ -1,3 +1,15 @@
+-- Criar o banco de dados se não existir
+DO $$ 
+BEGIN
+   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'contracts') THEN
+      CREATE DATABASE contracts;
+   END IF;
+END $$;
+
+-- Conectar ao banco de dados contracts (essa parte pode variar conforme o cliente SQL que você está usando)
+\c contracts;
+
+-- Criar a tabela proposals se não existir
 CREATE TABLE IF NOT EXISTS proposals (
     id SERIAL PRIMARY KEY,
     proposal_id UUID NOT NULL,
